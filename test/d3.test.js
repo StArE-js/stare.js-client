340dba047d7fd3fdf59c6d7210aa87ab75c88229
Diego Salazar S <diego.salazar.se@usach.cl>
2020-12-29 00:33:20 -0300

    CHG: Refactor code for test

--- test/d3.test.js
+++ test/d3.test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const debug = require('debug')('stare.js:client/test/d3.test.js');
 const _ = require('lodash');
 
 // const d3 = require('../lib/d3');
--- test/three.test.js
+++ test/three.test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const debug = require('debug')('stare.js:client/test/three.test.js');
 const _ = require('lodash');
 
 // const three = require('../lib/three');
